47000934239bdeda77bb8d2c85f968a04ff0762e
shangtianxuanniao <sun12378@qq.com>
2025-05-13 12:14:18 +0800

    finial

--- test.py
+++ test.py
@@ -5,20 +5,20 @@ import time
 import threading
 import matplotlib.pyplot as plt
 
-#捕获摄像头
+# 捕获摄像头
 cap = cv2.VideoCapture(0)
 
 # 设置摄像头参数，第一个和第二个为像素大小，第三个表示帧率
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 cap.set(cv2.CAP_PROP_FPS, 17)
 
-#树莓派输出控制
+# 树莓派输出控制
 EA, I2, I1, EB, I4, I3, LS, RS = (13, 19, 26, 16, 20, 21, 6, 5)
 FREQUENCY = 50
 GPIO.setmode(GPIO.BCM)
 GPIO.setup([EA, I2, I1, EB, I4, I3], GPIO.OUT)
-GPIO.setup([LS, RS],GPIO.IN)
+GPIO.setup([LS, RS], GPIO.IN)
 GPIO.output([EA, I2, EB, I3], GPIO.LOW)
 GPIO.output([I1, I4], GPIO.HIGH)
 
@@ -31,6 +31,13 @@ lspeed = 0
 rspeed = 0
 lcounter = 0
 rcounter = 0
+
+
+def delay_microseconds(us):
+    start = pi.get_current_tick()
+    while (pi.get_current_tick() - start) < us:
+        pass
+
 
 class PID:
 
@@ -68,9 +75,10 @@ class PID:
         """Determines how aggressively the PID reacts to the current error with setting Derivative Gain"""
         self.Kd = derivative_gain
 
+
 class PD:
 
-    def __init__(self, P=0, D=0, location=240):  #120
+    def __init__(self, P=0, D=0, location=240):  # 120
 
         self.Kp = P
         self.Kd = D
@@ -81,12 +89,10 @@ class PD:
 
     def update(self, feedback_value):
         self.err_pre = feedback_value - self.ideal_location
-        self.u = self.Kp * self.err_pre +  self.Kd * (self.err_pre - self.err_last)
+        self.u = self.Kp * self.err_pre + self.Kd * (self.err_pre - self.err_last)
         self.err_last = self.err_pre
         if self.u > 100:
             self.u = 100
-        elif self.u < 0:
-            self.u = 0
         return self.u
 
     def setKp(self, proportional_gain):
@@ -106,6 +112,7 @@ def my_callback(channel):
     elif (channel == RS):
         rcounter += 1
 
+
 def getspeed():
     global rspeed
     global lspeed
@@ -120,18 +127,19 @@ def getspeed():
         lcounter = 0
         time.sleep(0.1)
 
+
 thread1 = threading.Thread(target=getspeed)
 thread1.start()
 
-l_origin_duty = 5
-r_origin_duty = 5
+l_origin_duty = 0
+r_origin_duty = 0
 pwma.start(l_origin_duty)
 pwmb.start(r_origin_duty)
-L_control = PID(40, 0, 10, 0.1, l_origin_duty)
-R_control = PID(40, 0, 11, 0.13, r_origin_duty)
+L_control = PID(40, 0, 10, 0, l_origin_duty)
+R_control = PID(40, 0, 11, 0, r_origin_duty)
 
-#图像PD管理函数
-Control = PD(0.6,0.1,320)
+# 图像PD管理函数
+Control = PD(0.002, 0.002, 160)
 
 """
 在OpenCV中，HSV（色相、饱和度、明度）是一种常用的颜色空间，它可以方便地进行颜色的识别和处理。
@@ -166,18 +174,21 @@ higher_color = np.array([80, 200, 255])
 
 """
 
-Red_Eable = 3;
-Yellow_Eable = 4;
-Green_Eable = 5;
+Red_Eable = 3
+Yellow_Eable = 4
+Green_Eable = 5
 
 # 预定义公共内核
 kernel_5x5 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+
 
 def color_processing(hsv_img, lower, upper, max_contours=1):
     """通用颜色处理函数"""
     mask = cv2.inRange(hsv_img, lower, upper)
     dilate = cv2.dilate(mask, kernel_5x5, iterations=1)
-    _ ,cnts, _ = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    _, cnts, _ = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
     if not cnts:
         return ()  # 返回空元组
 
@@ -194,58 +205,56 @@ def color_processing(hsv_img, lower, upper, max_contours=1):
 
 # 修改后的颜色识别函数（保持原有注释不变）
 def Red_Identify(hsv):
-
     if Red_Eable != 3:
-        return 0,0,0,0
-    else:
-        # 二值化处理，表示HSV中颜色的范围
-        lower_color = np.array([0, 125, 100])
-        higher_color = np.array([10, 255, 255])
+        return 0, 0, 0, 0
 
-        result = color_processing(hsv, lower_color, higher_color)
-        return result if result else (0, 0, 0, 0)
+    # 二值化处理，表示HSV中颜色的范围
+    lower_color = np.array([0, 50, 50])
+    higher_color = np.array([10, 255, 255])
+
+    result = color_processing(hsv, lower_color, higher_color)
+    return result if result else (0, 0, 0, 0)
 
 
 def Yellow_Identify(hsv):
-
     if Yellow_Eable != 3:
-        return 0,0,0,0
-    else:
-        # 二值化处理，表示HSV中颜色的范围
-        lower_color = np.array([15, 70, 100])
-        higher_color = np.array([40, 200, 255])
+        return 0, 0, 0, 0
 
-        result = color_processing(hsv, lower_color, higher_color)
-        return result if result else (0, 0, 0, 0)
+    # 二值化处理，表示HSV中颜色的范围
+    lower_color = np.array([20, 65, 120])
+    higher_color = np.array([40, 230, 230])
+
+    result = color_processing(hsv, lower_color, higher_color)
+    return result if result else (0, 0, 0, 0)
 
 
 def Greem_Identify(hsv):
-    if Green_Eable != 5:
-        return 0,0,0,0
-    else:
-        # 二值化处理，表示HSV中颜色的范围
-        lower_color = np.array([40, 40, 40])
-        higher_color = np.array([90, 255, 255])
+    if Green_Eable != 3:
+        return 0, 0, 0, 0, 0, 0, 0, 0
 
-        results = color_processing(hsv, lower_color, higher_color, max_contours=2)
+    # 二值化处理，表示HSV中颜色的范围
+    lower_color = np.array([60, 45, 50])
+    higher_color = np.array([80, 205, 235])
 
-        if not results:
-            return (0, 0, 0, 0, 0, 0, 0, 0)
+    results = color_processing(hsv, lower_color, higher_color, max_contours=2)
+    if not results:
+        return (0, 0, 0, 0, 0, 0, 0, 0)
 
-        if len(results) == 1:
-            x, y, w, h = results[0]
-            return (x, y, w, h, 0, 0, 0, 0)
+    if len(results) == 1:
+        x, y, w, h = results[0]
+        return (x, y, w, h, 0, 0, 0, 0)
 
-        # 按面积排序返回结果
-        (x1, y1, w1, h1), (x2, y2, w2, h2) = sorted(results,
-                                                    key=lambda r: r[2] * r[3],
-                                                    reverse=True)
-        return (x1, y1, w1, h1, x2, y2, w2, h2)
+    # 按面积排序返回结果
+    (x1, y1, w1, h1), (x2, y2, w2, h2) = sorted(results,
+                                                key=lambda r: r[2] * r[3],
+                                                reverse=True)
+    return (x1, y1, w1, h1, x2, y2, w2, h2)
 
-def MaxClolor_Identify(img,hsv):
+
+def MaxClolor_Identify(img, hsv):
     # 优先检测红色
     x0, y0, w0, h0 = Red_Identify(hsv)
-    if w0 * h0 > 10000:  # 红色检测成功
+    if w0 * h0 > 500:  # 红色检测成功
         # 绘制红色框
         cv2.rectangle(img, (x0, y0), (x0 + w0, y0 + h0), (0, 0, 255), 3)
         cv2.circle(img, (x0 + (w0 // 2), y0 + (h0 // 2)), 6, (0, 0, 255), 2)
@@ -254,22 +263,24 @@ def MaxClolor_Identify(img,hsv):
 
     # 红色未检测到，检测黄色
     x1, y1, w1, h1 = Yellow_Identify(hsv)
-    if w1 * h1 > 10000:  # 黄色检测成功
+    if w1 * h1 > 800:  # 黄色检测成功
         # 绘制黄色框
         cv2.rectangle(img, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 255), 3)
-        cv2.circle( img, (x1 + (w1 // 2), y1 + (h1 // 2)), 6, (0, 255, 255), 2 )
+        cv2.circle(img, (x1 + (w1 // 2), y1 + (h1 // 2)), 6, (0, 255, 255), 2)
         cv2.imshow("identify", img)
         return 2, x1, w1, h1
 
     # 黄红均未检测到，检测绿色
     gx1, gy1, gw1, gh1, gx2, gy2, gw2, gh2 = Greem_Identify(hsv)
-    if gw1 * gh1 > 10000 :  # 绿色检测成功
+    if gw1 * gh1 > 800:  # 绿色检测成功
         # 绘制绿色框（可能两个）
         cv2.rectangle(img, (gx1, gy1), (gx1 + gw1, gy1 + gh1), (0, 255, 0), 3)
         cv2.circle(img, (gx1 + (gw1 // 2), gy1 + (gh1 // 2)), 6, (0, 255, 0), 2)
-        if gw2 * gh2 > 8000:  # 存在第二个绿色区域
+        if gw2 * gh2 > 800:  # 存在第二个绿色区域
             cv2.rectangle(img, (gx2, gy2), (gx2 + gw2, gy2 + gh2), (0, 255, 0), 3)
             cv2.circle(img, (gx2 + (gw2 // 2), gy2 + (gh2 // 2)), 6, (0, 255, 0), 2)
+            cv2.imshow("identify", img)
+            return 3, gx1, 0, gh1
         cv2.imshow("identify", img)
         return 3, gx1, gx2, gh1
 
@@ -277,46 +288,160 @@ def MaxClolor_Identify(img,hsv):
     cv2.imshow("identify", img)
     return 0, 0, 0, 0
 
-def PID_Set(img,hsv):
-    MAXH = 400
-    flag,x1,w1,h = MaxClolor_Identify(img,hsv)
-    if(flag == 1):
-        if(h>MAXH):
 
-            L_control.ideal_speed = 0
-            R_control.ideal_speed = 0.2
+Turn_flag = 0
+
+
+def PID_Set(img, hsv):
+    MAXH = 160
+    global Turn_flag
+
+    global Red_Eable
+    global Yellow_Eable
+    global Green_Eable
+
+    flag, x1, w1, h = MaxClolor_Identify(img, hsv)
+    if (Red_Eable * Yellow_Eable * Green_Eable == 0):
+
+        pwma.ChangeDutyCycle(40)
+        pwmb.ChangeDutyCycle(40)
+
+    elif (flag == 1):
+        Control.ideal_location = 160
+        if (h > MAXH):
+
+            pwma.ChangeDutyCycle(0)
+            pwmb.ChangeDutyCycle(40)
+
+            time.sleep(0.4)
+
+            pwma.ChangeDutyCycle(45)
+            pwmb.ChangeDutyCycle(45)
+            time.sleep(0.4)
+
+            Turn_flag = 1
+            Red_Eable = Red_Eable - 1
+            Yellow_Eable = Yellow_Eable - 1
+            Green_Eable = Green_Eable - 1
+
+            pwma.ChangeDutyCycle(40)
+            pwmb.ChangeDutyCycle(22)
+
+
+        else:
+
+            L_control.ideal_speed = 0.9 + Control.update(x1 + w1 / 2)
+            R_control.ideal_speed = 0.9 - Control.update(x1 + w1 / 2)
 
             pwma.ChangeDutyCycle(L_control.update(lspeed))
             pwmb.ChangeDutyCycle(R_control.update(rspeed))
+    elif (flag == 2):
+        Control.ideal_location = 160
+
+        if (h > MAXH - 10):
+
+            pwma.ChangeDutyCycle(40)
+            pwmb.ChangeDutyCycle(0)
 
             time.sleep(0.5)
 
-            L_control.ideal_speed = 0.3
-            R_control.ideal_speed = 0.2
+            pwma.ChangeDutyCycle(45)
+            pwmb.ChangeDutyCycle(45)
+            time.sleep(0.5)
 
-            pwma.ChangeDutyCycle(L_control.update(lspeed))
-            pwmb.ChangeDutyCycle(R_control.update(rspeed))
+            Turn_flag = 2
+            Red_Eable = Red_Eable - 1
+            Yellow_Eable = Yellow_Eable - 1
+            Green_Eable = Green_Eable - 1
+
+            pwma.ChangeDutyCycle(20)
+            pwmb.ChangeDutyCycle(40)
+
 
         else:
-            Control.ideal_location = 120
 
-            L_control.ideal_speed = 0.10 + Control.update(x1)
-            R_control.ideal_speed = 0.13 - Control.update(x1)
+            L_control.ideal_speed = 0.9 + Control.update(x1 + w1 / 2)
+            R_control.ideal_speed = 0.9 - Control.update(x1 + w1 / 2)
 
             pwma.ChangeDutyCycle(L_control.update(lspeed))
             pwmb.ChangeDutyCycle(R_control.update(rspeed))
-#
-#     else:
-#         if(h>MAXH):
-#             if(x1>240):
-#                 PID_RIGHT()
-#             else if(x1<240)
-#                 PID_LEFT()
-#         else:
-#             if(x1+x2<gXmax):
-#                 PID_LEFT()
-#             else if(x1+x2>gXmax):
-#                 PID_RIGHT()
+    elif (flag == 3):
+        if (Turn_flag == 1):
+            if (w1 != 0):
+                Control.ideal_location = 360
+
+                L_control.ideal_speed = 0.9 + Control.update(x1 / 2 + w1 / 2)
+                R_control.ideal_speed = 0.9 - Control.update(x1 / 2 + w1 / 2)
+
+                pwma.ChangeDutyCycle(L_control.update(lspeed))
+                pwmb.ChangeDutyCycle(R_control.update(rspeed))
+            else:
+                if (h > MAXH - 15):
+                    pwma.ChangeDutyCycle(35)
+                    pwmb.ChangeDutyCycle(0)
+
+                    time.sleep(0.5)
+
+                    pwma.ChangeDutyCycle(40)
+                    pwmb.ChangeDutyCycle(40)
+                    time.sleep(0.4)
+
+                    Red_Eable = Red_Eable - 1
+                    Yellow_Eable = Yellow_Eable - 1
+                    Green_Eable = Green_Eable - 1
+
+                    pwma.ChangeDutyCycle(20)
+                    pwmb.ChangeDutyCycle(40)
+                    time.sleep(0.3)
+
+                else:
+                    Control.ideal_location = 140
+
+                    L_control.ideal_speed = 0.9 + Control.update(x1)
+                    R_control.ideal_speed = 0.9 - Control.update(x1)
+
+                    pwma.ChangeDutyCycle(L_control.update(lspeed))
+                    pwmb.ChangeDutyCycle(R_control.update(rspeed))
+        if (Turn_flag == 2):
+            if (w1 != 0):
+                Control.ideal_location = -40
+
+                L_control.ideal_speed = 0.9 + Control.update(x1 / 2 + w1 / 2)
+                R_control.ideal_speed = 0.9 - Control.update(x1 / 2 + w1 / 2)
+
+                pwma.ChangeDutyCycle(L_control.update(lspeed))
+                pwmb.ChangeDutyCycle(R_control.update(rspeed))
+            else:
+                if (h > MAXH - 15):
+                    pwma.ChangeDutyCycle(0)
+                    pwmb.ChangeDutyCycle(35)
+
+                    time.sleep(0.5)
+
+                    pwma.ChangeDutyCycle(45)
+                    pwmb.ChangeDutyCycle(45)
+                    time.sleep(0.4)
+
+                    Red_Eable = Red_Eable - 1
+                    Yellow_Eable = Yellow_Eable - 1
+                    Green_Eable = Green_Eable - 1
+                    Turn_flag = 1
+
+                    pwma.ChangeDutyCycle(40)
+                    pwmb.ChangeDutyCycle(22)
+                    time.sleep(0.3)
+
+                else:
+                    Control.ideal_location = 180
+
+                    L_control.ideal_speed = 0.9 + Control.update(x1)
+                    R_control.ideal_speed = 0.9 - Control.update(x1)
+
+                    pwma.ChangeDutyCycle(L_control.update(lspeed))
+                    pwmb.ChangeDutyCycle(R_control.update(rspeed))
+
+    else:
+        a = 0
 
 
 while True:
@@ -327,14 +452,15 @@ while True:
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # 获取图像并调整PID
-    MaxClolor_Identify(img, hsv)
-    # PID_Set(img,hsv)
+    PID_Set(img, hsv)
+
     key = cv2.waitKey(1) & 0xFF  # 检测键盘,最长等待1ms
     if key == ord('q'):
         break  # 按q时结束
-# pwma.stop()
-# pwmb.stop()
-# GPIO.cleanup()
+
+pwma.stop()
+pwmb.stop()
+GPIO.cleanup()
 
 # 关闭摄像头，解除程序占用摄像头
 cap.release()
